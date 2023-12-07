c19342164f6ce132627bcc7e37f0f62187660b27
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-12-07 18:42:33 -0400

    Funcionalidad del Menú + CSS

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,24 +1,33 @@
+import { useState } from 'react';
+
 import IconoMenu from './media/iconos/bars-solid.svg';
 import IconoX from './media/iconos/x-solid.svg';
 import IconoClaro from './media/iconos/sun-solid.svg'
 
 export function Header({enlaces}) {
-    return <>
-        <section>
-            <button> <img src={IconoMenu} alt='Abrir Menú'/></button>
+
+    const [menu, setMenu] = useState(false);
+
+    function MenuAbrir() {
+        setMenu(true);
+    }
+    function MenuCerrar() {
+        setMenu(false);
+    }
+
+    return <header>
+        <section className='header-principal'>
+            <button onClick={() => MenuAbrir()}> <img src={IconoMenu} alt='Abrir Menú'/></button>
             <h1>TusPelis</h1>
             <button> <img src={IconoClaro} alt='Abrir Menú'/></button>
         </section>
-        <section>
-            <button> <img src={IconoX} alt='Cerrar Menú'/> </button>
+        <section className={menu === true ? 'menu abierto' : 'menu cerrado'}>
+            <button onClick={() => MenuCerrar()}> <img src={IconoX} alt='Cerrar Menú'/> </button>
             <h3 onClick={()=>enlaces(1)}>Inicio</h3>
             <h3 onClick={()=>enlaces(2)}>Buscar</h3>
             <h3>Explorar</h3>
             <h3>Contacto</h3>
         </section>
-    </>
-}
-
-function MenuAbrir() {
-
+        {menu === true ? <div className='fondo'></div> : <></>}
+    </header>
 }
