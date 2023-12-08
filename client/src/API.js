1db81eec39b281664c6de7f42f9b9ddb2ba4c6cf
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-12-07 22:01:18 -0400

    Barra de Busqueda
    -Devuelve los primeros 6 resultados
    -Aún no esta renderizado los resultados, solo se muestran por consola
    -No tiene estilos

--- client/src/API.js
+++ client/src/API.js
@@ -1,28 +1,36 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODg1ZmI5ZGUxN2U5OTNiMzkxZDJiOTk4ZDYxZTNjNyIsInN1YiI6IjY0YTJlOTExZThkMDI4MDExY2Y4ZDc0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UFrQpVBqiYKbi9FEPst6wTFWA45YbIRKNYtJGvxlRuk'
+    }
+};
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODg1ZmI5ZGUxN2U5OTNiMzkxZDJiOTk4ZDYxZTNjNyIsInN1YiI6IjY0YTJlOTExZThkMDI4MDExY2Y4ZDc0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UFrQpVBqiYKbi9FEPst6wTFWA45YbIRKNYtJGvxlRuk'
-//     }
-// };
+let idioma = "es-MX"
 
-// let idioma = "es-MX"
-
-// export function VerPelis() {
-//     TodasPelis()
-// }
-
-// async function TodasPelis() { 
-//     try {
-//         const responsePelis = await fetch(`https://api.themoviedb.org/3/discover/movie?language=${idioma}`, options);
-//         todas = await responsePelis.json();
-//         console.log("se ejecuto todasPelis")
-//         return <>
-//             <p>hay peliculas</p>
-//             {console.log("soy todas", todas)}
-//         </>
-//     } catch (err) {
-//         console.error(err);
-//     } 
-// }
+export async function APIbuscar(titulo) { 
+    try {
+        const responsePelis = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(titulo)}&language=${idioma}`, options);
+        const response = await responsePelis.json();
+        // console.log(response);
+        const todas = response.results;
+        const busqueda = todas.slice(0, 6);
+        // console.log("soy busqueda", typeof(busqueda), busqueda);
+        return busqueda;
+    } catch (err) {
+        console.error(err);
+    } 
+}
+export async function API() { 
+    try {
+        const responsePelis = await fetch(`https://api.themoviedb.org/3/discover/movie?language=${idioma}`, options);
+        const todas = await responsePelis.json();
+        console.log("se ejecuto todasPelis")
+        return <>
+            <p>hay peliculas</p>
+            {console.log("soy todas", todas)}
+        </>
+    } catch (err) {
+        console.error(err);
+    } 
+}
--- client/src/Buscador.js
+++ client/src/Buscador.js
@@ -1,16 +1,40 @@
+import { useState , useEffect } from "react";
+
+import { APIbuscar } from "./API";
+
 export function Buscar() {
-    console.log('buscaaar')
-    return <p>ESTAMOS EN BUSCAR</p>
+    // console.log('buscaaar')
+    const [resultado, setResultado] = useState([]);
+    const [valor, setValor] = useState("");
+
+    //Si ocurre algun cambio se ejecuta
+    useEffect(() => {
+        const timer = setTimeout(() => { //esperamos dos segundos por si hay un cambio
+            // console.log('vamos a la API');
+            handleChange(); //mandamos valor
+        }, 2000);
+    
+        return () => { //al cambiar el titulo(valor)
+            console.log('hubo un cambio')
+            clearTimeout(timer); //limpiamos tiempo
+            setResultado([]); //limpiamos resultados
+        };
+    }, [valor]);
+
+    //función para enviar titulo(valor) a la API
+    async function handleChange() {
+        if (valor) {
+            console.log('enviando valor a la api')
+            const pelis = await APIbuscar(valor);
+            console.log(pelis)
+            setResultado(pelis)
+        } else {
+            console.log('no hay un valor en el input')
+        }
+    }
+
+    return <>
+        <p>ESTAMOS EN BUSQUEDA</p>
+        <input type="text" placeholder="Ingresa el título de la película" value={valor} onChange={(e) => {setValor(e.target.value)}}></input>
+    </>
 }
-
-
-
-// import {VerPelis} from './API'
-
-// const titulo = "Luna Miel";
-// let estado = "buscarTitulo";
-// export function Buscar(){
-//     return <>
-//         <button onClick={() => VerPelis(titulo, estado)} >PRESIONAME</button>
-//     </>
-// }
